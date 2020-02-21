8f3973b7ab43cf5e433ee819e426273d255a9c5f
Carlos Eduardo <carloseduardoti@hotmail.com>
2020-02-21 10:41:16 -0300

    montando ambiente no eclipse

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,14 +1,15 @@
+import java.util.List;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.TelegramBotAdapter;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ChatAction;
 import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.request.SendChatAction;
+import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
 import com.pengrad.telegrambot.response.SendResponse;
-import org.apache.log4j.PropertyConfigurator;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MainClass {
     public static void main(String[] args) {
@@ -21,7 +22,7 @@ public class MainClass {
         //Responsavel por gerenciar o arquivo de respostas
         SendResponse sendResponse;
 
-        //Responsavel por gerenciar o envio de aÃ§oes do chat
+        //Responsavel por gerenciar o envio de açoes do chat
         BaseResponse baseResponse;
 
         // controle de off-set, a partir desse id sera feita a leitura das mensagens
@@ -32,6 +33,32 @@ public class MainClass {
             //ex comando no telegram para obter mensagens pendentes
             updatesResponse = bot.execute(new GetUpdates().limit(100).offset(m));
 
+            //ista de mensagens
+            List<Update> updates = updatesResponse.updates();
+
+            //analise de cada ação da mensagem
+            if(updates != null) {
+	            for(Update update : updates){
+	                //atualizando o offset
+	                m=update.updateId()+1;
+	
+	                System.out.println("Recebendo Mensagem: "+update.message().text());
+	
+	                //envio de "Escrevendo" antes de enviar a resposta
+	                baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+	
+	                //verificação de ação de chat foi enviada com sucesso
+	                System.out.println("Resposta de Chat Action Enviada?" + baseResponse.isOk());
+	
+	                //envio da mensagem de resposta
+	
+	                sendResponse = bot.execute(new SendMessage(update.message().chat().id(),"Não entendi, por favor repita!"));
+	
+	                //verificação de mensagem enviada com sucesso
+	                System.out.println("Mensagem Enviada?" +sendResponse.isOk());
+	
+	            }
+           }
         }
 
 
